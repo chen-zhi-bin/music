e90c25d7d9ce2460f5508ff5319051df6bf0fd83
chen-zhi-bin <815172923@qq.com>
2023-09-01 20:51:05 +0800

    android在音乐播放页添加音频可视化的动效，更换歌词显示框架

--- music-android-java/module_home/src/main/java/com/czb/module_home/ui/activity/MusicPlayActivity.java
+++ music-android-java/module_home/src/main/java/com/czb/module_home/ui/activity/MusicPlayActivity.java
@@ -1,20 +1,40 @@
 package com.czb.module_home.ui.activity;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Observer;
 
+import android.Manifest;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.drawable.Drawable;
+import android.media.audiofx.Visualizer;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
 import com.czb.module_base.RoutePath;
 import com.czb.module_base.base.BaseActivity;
 import com.czb.module_base.common.Constants;
@@ -27,9 +47,10 @@ import com.czb.module_home.callback.IMusicPlayActivityCallback;
 import com.czb.module_home.model.bean.MusicAndMusicianInfoBean;
 import com.czb.module_home.presenter.IMusicPlayActivityPresenter;
 import com.czb.module_home.utils.PresenterManager;
-import com.dirror.lyricviewx.LyricEntry;
-import com.dirror.lyricviewx.LyricViewX;
-import com.dirror.lyricviewx.OnPlayClickListener;
+import com.example.viewlibrary.util.AudioVisualConverter;
+import com.example.viewlibrary.util.BlurUtil;
+import com.example.viewlibrary.util.ImageUtil;
+import com.example.viewlibrary.view.JinyunView;
 import com.lzx.starrysky.OnPlayProgressListener;
 import com.lzx.starrysky.SongInfo;
 import com.lzx.starrysky.StarrySky;
@@ -44,6 +65,10 @@ import java.util.List;
 import java.util.Map;
 
 import io.reactivex.rxjava3.subjects.BehaviorSubject;
+import jp.wasabeef.glide.transformations.BlurTransformation;
+import me.wcy.lrcview.LrcView;
+import permison.PermissonUtil;
+import permison.listener.PermissionListener;
 
 @Route(path = RoutePath.Home.DETAIL_MUSIC)
 public class MusicPlayActivity extends BaseActivity implements IMusicPlayActivityCallback {
@@ -62,9 +87,15 @@ public class MusicPlayActivity extends BaseActivity implements IMusicPlayActivit
     private ConstraintLayout mHeaderLayout;
     private TextView mMusicNameTv;
     private TextView mMusicianNameTv;
-    private LyricViewX mLyricViewX;
+//    private LyricViewX mLyricViewX;
     private IMusicPlayActivityPresenter mMusicPlayActivityPresenter;
     private ImageView mCommentIv;
+    private LrcView mLrcView;
+    private RelativeLayout mJingyunLayout;
+    private ImageView mIvShowPic;
+    private JinyunView mJingyunView;
+    ObjectAnimator objectAnimator;
+    private ImageView mBgIv;
 
     @Override
     protected void initPresenter() {
@@ -127,21 +158,53 @@ public class MusicPlayActivity extends BaseActivity implements IMusicPlayActivit
                 }
                 if (mControl.isPlaying()) {
                     mControl.pauseMusic();
+                    onPause();
                 }else if (mControl.isPaused()){
                     mControl.restoreMusic();
+                    onResume();
                 }
             }
         });
-        mLyricViewX.setDraggable(true, new OnPlayClickListener() {
+//        mLyricViewX.setDraggable(true, new OnPlayClickListener() {
+//            @Override
+//            public boolean onPlayClick(long l) {
+//               //点击左边箭头时音乐跳转
+//                long s = l / 1000;
+//                mCurrentPosition.setText(mMinFormt.format(s));
+//                mLyricViewX.updateTime(l,true);
+//                mDurationBar.setProgress(Integer.parseInt(s+""));
+//                mControl.seekTo(l,true);
+//                LyricEntry currentLineLyricEntry = mLyricViewX.getCurrentLineLyricEntry();
+//                return true;
+//            }
+//        });
+        mIvShowPic.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onPlayClick(long l) {
-               //点击左边箭头时音乐跳转
-                long s = l / 1000;
+            public void onClick(View v) {
+                if (mJingyunLayout.getVisibility()==View.VISIBLE){
+                    mJingyunLayout.setVisibility(View.GONE);
+                    mLrcView.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        mLrcView.setOnTapListener(new LrcView.OnTapListener() {
+            @Override
+            public void onTap(LrcView view, float x, float y) {
+                if (mLrcView.getVisibility()==View.VISIBLE){
+                    mJingyunLayout.setVisibility(View.VISIBLE);
+                    mLrcView.setVisibility(View.GONE);
+                }
+            }
+        });
+        mLrcView.setDraggable(true, new LrcView.OnPlayClickListener() {
+            @Override
+            public boolean onPlayClick(LrcView view, long time) {
+                //点击左边箭头时音乐跳转
+                long s = time / 1000;
                 mCurrentPosition.setText(mMinFormt.format(s));
-                mLyricViewX.updateTime(l,true);
+                mLrcView.updateTime(time);
                 mDurationBar.setProgress(Integer.parseInt(s+""));
-                mControl.seekTo(l,true);
-                LyricEntry currentLineLyricEntry = mLyricViewX.getCurrentLineLyricEntry();
+                mControl.seekTo(time,true);
                 return true;
             }
         });
@@ -154,7 +217,8 @@ public class MusicPlayActivity extends BaseActivity implements IMusicPlayActivit
                 mDurationBar.setProgress(Integer.parseInt(s+""));
 
                 //刷新歌词
-                mLyricViewX.updateTime(currPos,true);
+//                mLyricViewX.updateTime(currPos,true);
+                mLrcView.updateTime(currPos);
             }
         });
 
@@ -200,24 +264,25 @@ public class MusicPlayActivity extends BaseActivity implements IMusicPlayActivit
     @Override
     protected void initView() {
         mHeaderLayout = this.findViewById(R.id.home_hear_layout);
-        mLyricViewX = this.findViewById(R.id.lyricViewX);
-        mLyricViewX.setCurrentColor(  ContextCompat.getColor(
-                this,
-                R.color.colorWhite
-        ));
-        mLyricViewX.setLabel("暂无歌词");
-        mLyricViewX.setTimeTextColor(
-                ContextCompat.getColor(
-                        this,
-                        R.color.colorWhite
-                )
-        );
-        mLyricViewX.setTimelineColor(
-                ContextCompat.getColor(
-                        this,
-                        R.color.colorWhite
-                )
-        );
+//        mLyricViewX = this.findViewById(R.id.lyricViewX);
+//        mLyricViewX.setCurrentColor(  ContextCompat.getColor(
+//                this,
+//                R.color.colorWhite
+//        ));
+//        mLyricViewX.setLabel("暂无歌词");
+//        mLyricViewX.setTimeTextColor(
+//                ContextCompat.getColor(
+//                        this,
+//                        R.color.colorWhite
+//                )
+//        );
+//        mLyricViewX.setTimelineColor(
+//                ContextCompat.getColor(
+//                        this,
+//                        R.color.colorWhite
+//                )
+//        );
+        mLrcView = this.findViewById(R.id.lyricView);
         mPlayOrPauseIv = this.findViewById(R.id.play_pause);
         mLastMusicIv = this.findViewById(R.id.last_music);
         mNextMusicIv = this.findViewById(R.id.next_music);
@@ -230,8 +295,48 @@ public class MusicPlayActivity extends BaseActivity implements IMusicPlayActivit
         this.findViewById(R.id.back).setOnClickListener(v -> finish());
         setStatusBar();
         mControl = StarrySky.with();
-
+        ConstraintLayout layout = this.findViewById(R.id.layout);
+        layout.setBackground(null);
+        mBgIv = this.findViewById(R.id.bg_iv);
+        mBgIv.setDrawingCacheEnabled(true);
         SongInfo nowPlayingSongInfo = mControl.getNowPlayingSongInfo();
+        String songCover = nowPlayingSongInfo.getSongCover();
+
+        if (songCover != null) {
+            Glide.with(mBgIv.getContext())
+                    .load(songCover)
+                    .apply(RequestOptions.bitmapTransform(new BlurTransformation(35,3)))
+                    .listener(new RequestListener<Drawable>() {
+                        @Override
+                        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                            setBackground(songCover);
+                            return false;
+                        }
+                    })
+                    .into(mBgIv);
+        }else {
+            Glide.with(mBgIv.getContext())
+                    .load(R.mipmap.background)
+                    .listener(new RequestListener<Drawable>() {
+                        @Override
+                        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                            setBackground(null);
+                            return false;
+                        }
+                    })
+                    .into(mBgIv);
+        }
+//        setBackground(songCover);
         if (nowPlayingSongInfo!=null){
             long duration = nowPlayingSongInfo.getDuration();
             mDurationBar.setMax(Integer.parseInt(duration+""));
@@ -260,13 +365,50 @@ public class MusicPlayActivity extends BaseActivity implements IMusicPlayActivit
             mPlayOrPauseIv.setImageResource(R.mipmap.play_white);
         }
 
-    }
+        mIvShowPic = this.findViewById(R.id.ivShowPic);
+        mJingyunLayout = this.findViewById(R.id.jingyu_layout);
+        mJingyunView = this.findViewById(R.id.sv_bg);
+        Glide.with(MusicPlayActivity.this).asBitmap().addListener(new RequestListener<Bitmap>() {
+            @Override
+            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Bitmap> target, boolean isFirstResource) {
+                return false;
+            }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-//        getLyric();
+            @Override
+            public boolean onResourceReady(Bitmap resource, Object model, Target<Bitmap> target, DataSource dataSource, boolean isFirstResource) {
+//                mJingyunView.setmPaintColor(ImageUtil.getColor(resource, 3).getRgb());
+                return false;
+            }
+        }).load(songCover).circleCrop().into(mIvShowPic);
+        mIvShowPic.setClipToOutline(true);
+        mIvShowPic.setOutlineProvider(ImageUtil.getOutline(true, 20, 1));
+//
+        objectAnimator = ObjectAnimator.ofFloat(mIvShowPic, "rotation", 0f, 360f);
+        objectAnimator.setDuration(20 * 1000);
+        objectAnimator.setRepeatMode(ValueAnimator.RESTART);
+        objectAnimator.setInterpolator(new LinearInterpolator());
+        objectAnimator.setRepeatCount(-1);
+        objectAnimator.start();
+//        getBitmap();
+        PermissonUtil.checkPermission(this, new PermissionListener() {
+            @Override
+            public void havePermission() {
+                initVisualizer();
+            }
+            @Override
+            public void requestPermissionFail() {
+
+            }
+        }, permissons);
+
     }
+    String[] permissons = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.RECORD_AUDIO};
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+////        getLyric();
+//    }
 
     private void setStatusBar() {
         StatusBarUtil.immersive(this);
@@ -313,11 +455,112 @@ public class MusicPlayActivity extends BaseActivity implements IMusicPlayActivit
     @Override
     public void setMusicInfo(MusicAndMusicianInfoBean data) {
         String lyric = data.getData().getMusicInfo().getLyric();
-        mLyricViewX.loadLyric(lyric,null);
+//        mLyricViewX.loadLyric(lyric,null);
+        mLrcView.loadLrc(lyric);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        mMusicPlayActivityPresenter.unregisterViewCallback(this);
+    }
+
+    private void setBackground(String url) {
+        if (url==null) {
+            Bitmap bitmap = BlurUtil.doBlur(BitmapFactory.decodeResource(getResources(), R.mipmap.background), 10, 30);
+            mBgIv.setImageBitmap(bitmap);
+        }
+
+        mBgIv.setDrawingCacheEnabled(true);
+        getBitmap();
+
+        ColorMatrix colorMatrix = new ColorMatrix();
+        colorMatrix.setScale(0.7f, 0.7f, 0.7f, 1);
+        ColorMatrixColorFilter colorFilter = new ColorMatrixColorFilter(colorMatrix);
+        mBgIv.setColorFilter(colorFilter);
+
+    }
+
+    int top = 0;
+    int width = 0;
+    int height = 0;
+    public void getBitmap() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //启用DrawingCache并创建位图
+//                iv_bg.buildDrawingCache();
+//                while (iv_bg.getDrawingCache() == null) {
+//                    iv_bg.buildDrawingCache();
+//                    SystemClock.sleep(10);
+//                }
+                mBgIv.buildDrawingCache();
+                while (mBgIv.getDrawingCache() == null) {
+                    mBgIv.buildDrawingCache();
+                    SystemClock.sleep(10);
+                }
+//                Bitmap bitmap2 = Bitmap.createBitmap(mBgIv.getDrawingCache());
+//                Bitmap bitmap2 = Bitmap.createBitmap(iv_bg.getDrawingCache());
+                Bitmap bitmap2 = Bitmap.createBitmap(mBgIv.getDrawingCache());
+                top = top ==0 ?mJingyunView.getTop():top;
+                width = width ==0 ?mJingyunView.getWidth():width;
+                height = height ==0 ?mJingyunView.getHeight():height;
+//                bitmap2 = Bitmap.createBitmap(bitmap2, 0, top, width, height);
+                bitmap2 = Bitmap.createBitmap(bitmap2, 0, top, width, height);
+                mJingyunView.setBitmapBg(bitmap2);
+
+//                iv_bg.setDrawingCacheEnabled(false);
+                mBgIv.setDrawingCacheEnabled(false);
+            }
+        });
+        thread.start();
+        //暂时不显示
+        mJingyunLayout.setVisibility(View.GONE);
+    }
+    AudioVisualConverter visualConverter=new AudioVisualConverter();
+
+    private Visualizer.OnDataCaptureListener dataCaptureListener = new Visualizer.OnDataCaptureListener() {
+        @Override
+        public void onWaveFormDataCapture(Visualizer visualizer, final byte[] waveform, int samplingRate) {
+            mJingyunView.setmBytes(visualConverter.converter(waveform));
+        }
+
+        @Override
+        public void onFftDataCapture(Visualizer visualizer, final byte[] fft, int samplingRate) {
+
+        }
+    };
+    private Visualizer visualizer;
+    public void initVisualizer() {
+        if(visualizer != null){
+            visualizer = null;
+        }
+        visualizer = new Visualizer(0);
+        //采样的最大值
+        int captureSize = Visualizer.getCaptureSizeRange()[1];
+        //采样的频率
+        int captureRate = Visualizer.getMaxCaptureRate() * 2 / 3;
+        visualizer.setEnabled(false);
+        visualizer.setCaptureSize(captureSize);
+        visualizer.setDataCaptureListener(dataCaptureListener, captureRate, true, true);
+        visualizer.setScalingMode(Visualizer.SCALING_MODE_NORMALIZED);
+        visualizer.setMeasurementMode(Visualizer.MEASUREMENT_MODE_PEAK_RMS);
+        visualizer.setEnabled(true);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (objectAnimator.isPaused()) {
+            objectAnimator.resume();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (objectAnimator.isRunning()) {
+            objectAnimator.pause();
+        }
     }
 }
